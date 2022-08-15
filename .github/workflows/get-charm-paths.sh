d7d97d4638dedf02560a0c8c4dc7ffcefd1cbf63
Andrew Scribner <ca.scribner+1@gmail.com>
2022-08-15 15:40:07 -0400

    edit workflows

--- .github/workflows/get-charm-paths.sh
+++ .github/workflows/get-charm-paths.sh
@@ -10,20 +10,20 @@
 CHARMS_DIR="./charms"
 if [ -d "$CHARMS_DIR" ];
 then
-	CHARM_PATHS=$(find $CHARMS_DIR -maxdepth 1 -type d -not -path '*/\.*' -not -path "$CHARMS_DIR")
+  CHARM_PATHS=$(find $CHARMS_DIR -maxdepth 1 -type d -not -path '*/\.*' -not -path "$CHARMS_DIR")
 else
-	if [ -f "./metadata.yaml" ]
-	then
-		CHARM_PATHS="./"
-	else
-		echo "Cannot find valid charm directories - aborting"
-		exit 1
-	fi
+  if [ -f "./metadata.yaml" ]
+  then
+    CHARM_PATHS="./"
+  else
+    echo "Cannot find valid charm directories - aborting"
+    exit 1
+  fi
 fi
 
 # Convert output to JSON string format
 # { charm_paths: [...] }
-CHARM_PATHS_LIST=$(echo $CHARM_PATHS | jq -c --slurp --raw-input 'split("\n")[:-1]')
+CHARM_PATHS_LIST=$(echo "$CHARM_PATHS" | jq -c --slurp --raw-input 'split("\n")[:-1]')
 
 echo "Found CHARM_PATHS_LIST: $CHARM_PATHS_LIST"
 

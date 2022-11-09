ca38087d4972b08f052b1518980d82957e846af7
Andrew Scribner <ca.scribner+1@gmail.com>
2022-11-09 16:06:30 -0500

    feat: add renovate to manage repo (#23)
    * feat: init renovate
    
    * fix: renovate.yaml
    
    * fix: renovate.yaml

---
+++ .github/workflows/renovate-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "branchPrefix": "test-renovate/",
+    "dryRun": null,
+    "username": "renovate-release",
+    "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+    "onboarding": true,
+    "platform": "github",
+    "includeForks": true,
+    "repositories": ["ca-scribner/test-charm"]
+};
